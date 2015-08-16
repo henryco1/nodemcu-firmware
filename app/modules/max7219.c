79fbf370cf0c04b172782983aa857e7d01165f26
Konrad Beckmann <konrad.beckmann@gmail.com>
2015-08-16 11:28:13 +0200

    Works pretty great now

--- app/modules/max7219.c
+++ app/modules/max7219.c
@@ -5,16 +5,6 @@
 #include "lrotable.h"
 #include "c_stdlib.h"
 #include "c_string.h"
-#include "driver/spi.h"
-
-/**
- * Code is based on https://github.com/CHERTS/esp8266-devkit/blob/master/Espressif/examples/EspLightNode/user/ws2801.c
- * and provides a similar api as the ws2812 module.
- * The current implementation allows the caller to use
- * any combination of GPIO0, GPIO2, GPIO4, GPIO5 as clock and data.
- * Also supports HSPI (Hardware-SPI) which reduces CPU load a lot
- * and supports much higher bandwidth.
- */
 
 u32 max7219_dout = 2; // gpio4
 u32 max7219_cs = 3;   // gpio0
@@ -29,12 +19,9 @@ static void max7219_strip(u8 const *data, u32 len) {
     while (true) {
         if (*data & mask) {
             GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, dout_mask); // Set pin high
-            //c_printf("1");
         } else {
             GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, dout_mask); // Set pin low
-            //c_printf("0");
         }
-
 
         GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, clk_mask); // Set pin high
         GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, clk_mask); // Set pin low
@@ -53,10 +40,25 @@ static void max7219_write_reg(u8 const reg, u8 const data) {
     u8 buf[2] = {reg, data};
 
     GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << pin_num[max7219_cs]); // CS=low
-
     max7219_strip(buf, sizeof(buf));
-
     GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << pin_num[max7219_cs]); // CS=high
+}
+
+static int ICACHE_FLASH_ATTR max7219_write_lua(lua_State* L) {
+    size_t length;
+    const char *buffer = luaL_checklstring(L, 1, &length);
+
+    if (length < 2) {
+        return luaL_error(L, "wrong arg range");
+    }
+
+    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, 1 << pin_num[max7219_cs]); // CS=low
+    max7219_strip(buffer, length);
+    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, 1 << pin_num[max7219_cs]); // CS=high
+    
+    lua_pushinteger(L, length);
+
+    return 1;
 }
 
 static void enable_pin(u32 pin){
@@ -79,19 +81,6 @@ static int max7219_init_lua(lua_State* L) {
     return 1;
 }
 
-static int ICACHE_FLASH_ATTR max7219_write_lua(lua_State* L) {
-    size_t length;
-    const char *buffer = luaL_checklstring(L, 1, &length);
-
-    if (length != 2) {
-        return luaL_error(L, "wrong arg range");
-    }
-    max7219_write_reg(buffer[0], buffer[1]);
-
-    lua_pushinteger(L, length);
-
-    return 1;
-}
 
 #define MIN_OPT_LEVEL 2
 #include "lrodefs.h"
